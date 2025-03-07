f2f7f95d489241ac92431288aa501433dc0725c6
Samarth2151 <samarthpatane2151@gmail.com>
2025-03-07 10:51:33 +0530

    Add files via upload

---
+++ shapes.java
@@ -0,0 +1,38 @@
+abstract class shapes {
+    int dim1, dim2;
+
+    shapes(int a, int b) {
+        dim1 = a;
+        dim2 = b;
+    }
+
+    abstract void area();
+
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle(10, 5);
+        Triangle t = new Triangle(10, 5);
+
+        r.area();
+        t.area();
+    }
+}
+
+class Rectangle extends shapes {
+    Rectangle(int a, int b) {
+        super(a, b);
+    }
+
+    void area() {
+        System.out.println("Rectangle Area: " + (dim1 * dim2));
+    }
+}
+
+class Triangle extends shapes {
+    Triangle(int a, int b) {
+        super(a, b);
+    }
+
+    void area() {
+        System.out.println("Triangle Area: " + (0.5 * dim1 * dim2));
+    }
+}
