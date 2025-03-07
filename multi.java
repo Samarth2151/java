2030f0674e353007ec4016e9ab10b522e0c03426
Samarth2151 <samarthpatane2151@gmail.com>
2025-03-07 10:50:40 +0530

    Add files via upload

---
+++ multi.java
@@ -0,0 +1,26 @@
+class student{
+    int rollno=12;    
+}
+class test extends student{
+
+        int sub1=40;
+        int sub2=30;
+
+}
+class result extends test{
+
+    void displayresult(){
+        System.out.println("Roll number is "+rollno);
+        System.out.println("Got marks out of 50 in AOOC "+sub1);
+        System.out.println("Got marks out of 50 in Automata theory  "+sub2);
+        
+    }
+
+}
+
+class multi{
+     public static void main(String[] args){
+        result obj=new result();
+        obj.displayresult();
+    }
+}
